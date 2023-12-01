232cfa398c9e6885f0b6cbb488c5a30ea2602441
unknown <Bollingerr9@gmail.com>
2023-12-01 16:11:44 -0500

    style, navbar, and pages updates

--- flask_app/controllers/controller.py
+++ flask_app/controllers/controller.py
@@ -8,3 +8,11 @@ def show_home_page():
 @app.route('/privacy_policy')
 def show_privacy_policy():
     return render_template('privacy_policy.html')
+
+@app.route('/contact')
+def show_contact_info():
+    return render_template('contact.html')
+
+@app.route('/about_us')
+def show_about_us():
+    return render_template('about_us.html')
