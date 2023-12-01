aa1ae5802535164c21e625c39857f64e1bb5d71f
unknown <Bollingerr9@gmail.com>
2023-11-30 23:15:57 -0500

    Privacy Policy and Style Changes

--- flask_app/controllers/controller.py
+++ flask_app/controllers/controller.py
@@ -4,3 +4,7 @@ from flask_app import app
 @app.route('/')
 def show_home_page():
     return render_template('index.html')
+
+@app.route('/privacy_policy')
+def show_privacy_policy():
+    return render_template('privacy_policy.html')
